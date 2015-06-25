431d8e0b786ff029122fb10094353ae3bf5788db
PedroRajao <pedrorajao@gmail.com>
2015-06-25 18:03:46 -0300

    Create 22.bonfire-search-and-replace.js
    test

---
+++ 22.bonfire-search-and-replace.js
@@ -0,0 +1,11 @@
+function replace(str, find, replace){  
+   return str.replace(new RegExp(find, 'gi'), replace);  
+  var posBefore = str.indexOf(before);
+  if (posBefore charat){
+     uppercase
+     lowercase
+
+  }
+}
+
+replace("A quick brown fox jumped over the lazy dog", "Jumped", "leaped");
