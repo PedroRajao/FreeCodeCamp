5c491d211e4e98d30801a117830530eb4c151253
PedroRajao <pedrorajao@gmail.com>
2015-06-25 15:09:16 -0300

    Create 20.bonfire-diff-two-arrays.js

---
+++ 20.bonfire-diff-two-arrays.js
@@ -0,0 +1,17 @@
+function diff(arr1, arr2) {  
+  var newArr = [];
+  for (var i in arr1) {
+    if (arr2.indexOf(arr1[i]) === -1) {
+      newArr.push(arr1[i]);
+    }
+  }
+  for (var i in arr2) {
+    if (arr1.indexOf(arr2[i]) === -1) {
+      newArr.push(arr2[i]);
+    }
+  }
+  // Same, same; but different.
+  return newArr;
+}
+
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);  
