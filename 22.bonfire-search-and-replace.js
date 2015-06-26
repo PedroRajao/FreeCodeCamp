e3a804db67c15d9aeb8a50614dd34fa79c8ef1a7
PedroRajao <pedrorajao@gmail.com>
2015-06-26 13:07:42 -0300

    Update 22.bonfire-search-and-replace.js

--- 22.bonfire-search-and-replace.js
+++ 22.bonfire-search-and-replace.js
@@ -1,11 +1,26 @@
-function replace(str, find, replace){  
-   return str.replace(new RegExp(find, 'gi'), replace);  
-  var posBefore = str.indexOf(before);
-  if (posBefore charat){
-     uppercase
-     lowercase
+replace("He is Sleeping on the couch", "Sleeping", "sitting");
 
-  }
+function replace(str, find, replace) {   
+// Originals
+    var findOrig = find;
+    var strOrig = str;
+// TO COMPARISON    
+    find = find.toLowerCase();
+    str  = str.toLowerCase();
+// int :  first letter 
+    var posFirstLetter = str.indexOf(find);
+    posFirstLetter = parseInt(posFirstLetter);
+// char : first letter
+    var firstLetter = str.charAt(posFirstLetter);
+// replace without the capitalization     
+    var strReplaced = strOrig.replace(new RegExp(find, 'gi'), replace);
+// Capitalize, is it upper?
+    if (findOrig[0] == findOrig[0].toUpperCase()) {
+        strReplaced = capitalizeFirstLetter(strReplaced, posFirstLetter);
+    }
+    return strReplaced;
 }
 
-replace("A quick brown fox jumped over the lazy dog", "Jumped", "leaped");
+function capitalizeFirstLetter(string, pos_str) {
+    return string.substr(0, pos_str) + string.charAt(pos_str).toUpperCase() + string.slice(pos_str + 1);
+}
