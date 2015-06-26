12a420d6f3122ea1b9c781d453179c0c92e81c59
PedroRajao <pedrorajao@gmail.com>
2015-06-26 13:11:08 -0300

    Update 22.bonfire-search-and-replace.js

--- 22.bonfire-search-and-replace.js
+++ 22.bonfire-search-and-replace.js
@@ -14,10 +14,11 @@ function replace(str, find, replace) {
     var firstLetter = str.charAt(posFirstLetter);
 // replace without the capitalization     
     var strReplaced = strOrig.replace(new RegExp(find, 'gi'), replace);
-// Capitalize, is it upper?
+// Capitalize the new array if the original was in upper
     if (findOrig[0] == findOrig[0].toUpperCase()) {
         strReplaced = capitalizeFirstLetter(strReplaced, posFirstLetter);
     }
+// else do nothing    
     return strReplaced;
 }
 
